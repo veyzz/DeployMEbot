f2eb7461ca5db8bbca934a315b33850ba0ba8638
veyzz <munir.taga@gmail.com>
2019-06-09 14:39:11 +0300

    Добавил пример клавиатуры (меню), загрузку и сохранение файла

--- deploymebot.py
+++ deploymebot.py
@@ -3,6 +3,7 @@ import config
 import telebot
 from telebot import apihelper,types
 import cherrypy
+import os
 
 
 MODE = config.mode
@@ -23,16 +24,41 @@ class WebhookServer(object):
         return ''
 
 
-@bot.message_handler(commands=['help'])
+@bot.message_handler(commands=['start', 'help'])
 def _(message):
-    response = 'Помощи пока нет'
-    bot.send_message(message.chat.id, response)
+    response = '''Привет! Этот бот позволяет развернуть Вашего бота
+на сервере, чтобы держать его запущенным 24/7.'''
+    keyboard = types.ReplyKeyboardMarkup(True, True)
+    keyboard.row('Попробовать')
+    bot.send_message(message.chat.id, response, reply_markup=keyboard)
+
+
+@bot.message_handler(content_types=['document'])
+def _(message):
+    try:
+        file_info = bot.get_file(message.document.file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+        path = './download/{}/'.format(message.from_user.id)
+        if not os.path.exists(path):
+            os.makedirs(path)
+        path += message.document.file_name
+        with open(path, 'wb') as file:
+           file.write(downloaded_file)
+        bot.reply_to(message, "Принял Ваш файл!")
+    except Exception as e:
+        print(e)
 
 
 @bot.message_handler(content_types=["text"])
 def _(message):
-    response = 'Любое сообщение'
-    bot.send_message(message.chat.id, response)
+    response = 'Выберите интересующий Вас элемент меню:'
+    keyboard = types.InlineKeyboardMarkup()
+    button1 = types.InlineKeyboardButton(text="Панель управления", callback_data="panel")
+    button2 = types.InlineKeyboardButton(text="Загрузить бота", callback_data="upload")
+    button3 = types.InlineKeyboardButton(text="О проекте", callback_data="about")
+    keyboard.row(button1, button2)
+    keyboard.row(button3)
+    bot.send_message(message.chat.id, response, reply_markup=keyboard)
 
         
 if __name__ == '__main__':
