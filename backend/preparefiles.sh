d959b76f0a168440fc11eba02909ccd40b4f4860
glebasson <glebasson@yandex.ru>
2019-06-12 02:24:34 +0300

    create user folder from archive

--- backend/preparefiles.sh
+++ backend/preparefiles.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 # This script prepare file structure for new user's bot
 user_id=$1
-req=$2
-arch=$3
-mkdir $user_id
-if [ "$?" = 0 ]; then
-  # create virtualenv and install essensial modules
-  python3 -m venv ./$user_id/venv
-  source ./$user_id/venv/bin/activate
-  pip3 install -r $req
-  # extract files
-  mkdir ./$user_id/bot
-  tar -C ./$user_id/bot -xzvf $arch 
-else
-  exit 1
-fi
+arch=$2
+bot_name=${arch%.zip}
 
+# make new dir and clean old files
+mkdir ../bots/$user_id
+mkdir ../bots/$user_id/$bot_name
+rm -rf ../bots/$user_id/$bot_name/*
+
+# unzip files
+unzip $arch -d ../bots/$user_id/$bot_name
+dr=$(ls ../bots/$user_id/$bot_name)
+mv ../bots/$user_id/$bot_name/$dr/* ../bots/$user_id/$bot_name/
+# create virtualenv and install essensial modules
+python3 -m venv ../bots/$user_id/$bot_name/venv
+source ../bots/$user_id/$bot_name/venv/bin/activate
+pip3 install -r ../bots/$user_id/$bot_name/requirements.txt
+rm -rf ../bots/$user_id/$bot_name/$dr 
+exit 0
