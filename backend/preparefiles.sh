a6883a6e0c3b09cbb99556f88b5d1bc2bfdb8371
veyzz <munir.taga@gmail.com>
2019-06-12 20:30:09 +0300

    added generating sh files from templates

--- backend/preparefiles.sh
+++ backend/preparefiles.sh
@@ -15,12 +15,16 @@ rm -rf $pathtobot/bots/$user_id/$bot_name/*
 unzip $pathtobot/download/$user_id/$arch -d $pathtobot/bots/$user_id/$bot_name
 dr=$(ls $pathtobot/bots/$user_id/$bot_name)
 mv $pathtobot/bots/$user_id/$bot_name/$dr/* $pathtobot/bots/$user_id/$bot_name/
+
 # create virtualenv and install essensial modules
 python3 -m venv $pathtobot/bots/$user_id/$bot_name/venv
 source $pathtobot/bots/$user_id/$bot_name/venv/bin/activate
 pip3 install -r $pathtobot/bots/$user_id/$bot_name/requirements.txt
 rm -rf $pathtobot/bots/$user_id/$bot_name/$dr
-echo "#!/bin/bash" > $pathtobot/bots/$user_id/$bot_name/start.sh
-echo "venv/bin/python3 main.py > main.log 2>&1 &" >> $pathtobot/bots/$user_id/$bot_name/start.sh
-echo "echo \$! > main.pid" >> $pathtobot/bots/$user_id/$bot_name/start.sh
+
+# create sh files
+cat $pathtobot/backend/templates/start_bot > $pathtobot/bots/$user_id/$bot_name/start.sh
+cat $pathtobot/backend/templates/stop_bot > $pathtobot/bots/$user_id/$bot_name/stop.sh
+cat $pathtobot/backend/templates/check_process > $pathtobot/bots/$user_id/$bot_name/check_process.sh
+
 exit 0
