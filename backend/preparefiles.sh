7022ab08ed1784a1d5a5fefd4d701be39c5f629f
glebasson <glebasson@yandex.ru>
2019-06-12 04:04:26 +0300

    hot fix

--- backend/preparefiles.sh
+++ backend/preparefiles.sh
@@ -6,17 +6,18 @@ pathtobot=$3
 bot_name=${arch%.zip}
 
 # make new dir and clean old files
-mkdir $pathtobot/$user_id
-mkdir $pathtobot/$user_id/$bot_name
-rm -rf $pathtobot/$user_id/$bot_name/*
+echo $pathtobot/bots/$user_id
+mkdir $pathtobot/bots/$user_id
+mkdir $pathtobot/bots/$user_id/$bot_name
+rm -rf $pathtobot/bots/$user_id/$bot_name/*
 
 # unzip files
-unzip $arch -d $pathtobot/$user_id/$bot_name
-dr=$(ls $pathtobot/$user_id/$bot_name)
-mv $pathtobot/$user_id/$bot_name/$dr/* $pathtobot/$user_id/$bot_name/
+unzip $pathtobot/download/$user_id/$arch -d $pathtobot/bots/$user_id/$bot_name
+dr=$(ls $pathtobot/bots/$user_id/$bot_name)
+mv $pathtobot/bots/$user_id/$bot_name/$dr/* $pathtobot/bots/$user_id/$bot_name/
 # create virtualenv and install essensial modules
-python3 -m venv $pathtobot/$user_id/$bot_name/venv
-source $pathtobot/$user_id/$bot_name/venv/bin/activate
-pip3 install -r $pathtobot/$user_id/$bot_name/requirements.txt
-rm -rf $pathtobot/$user_id/$bot_name/$dr 
+python3 -m venv $pathtobot/bots/$user_id/$bot_name/venv
+source $pathtobot/bots/$user_id/$bot_name/venv/bin/activate
+pip3 install -r $pathtobot/bots/$user_id/$bot_name/requirements.txt
+rm -rf $pathtobot/bots/$user_id/$bot_name/$dr 
 exit 0
