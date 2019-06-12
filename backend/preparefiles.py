141fedcfb72ea9d4e95f79d970670836081cd107
glebasson <glebasson@yandex.ru>
2019-06-12 03:16:22 +0300

    fast fix

--- backend/preparefiles.py
+++ backend/preparefiles.py
@@ -3,9 +3,9 @@ import subprocess  # safelly run bash scripts from python
 import sys  # redirect stdout
 
 
-def deploy(user_id, arch):
+def deploy(user_id, arch, path):
     # run bash script and redirect stdout
-    sub = subprocess.run(["./preparefiles.sh", str(user_id), arch],
+    sub = subprocess.run(["./backend/preparefiles.sh", str(user_id), arch, path],
                          stdout=sys.stdout)
     if not sub.returncode:
         print("ok")
--- backend/preparefiles.sh
+++ backend/preparefiles.sh
@@ -2,20 +2,21 @@
 # This script prepare file structure for new user's bot
 user_id=$1
 arch=$2
+pathtobot=$3
 bot_name=${arch%.zip}
 
 # make new dir and clean old files
-mkdir ../bots/$user_id
-mkdir ../bots/$user_id/$bot_name
-rm -rf ../bots/$user_id/$bot_name/*
+mkdir $pathtobot/$user_id
+mkdir $pathtobot/$user_id/$bot_name
+rm -rf $pathtobot/$user_id/$bot_name/*
 
 # unzip files
-unzip $arch -d ../bots/$user_id/$bot_name
-dr=$(ls ../bots/$user_id/$bot_name)
-mv ../bots/$user_id/$bot_name/$dr/* ../bots/$user_id/$bot_name/
+unzip $arch -d $pathtobot/$user_id/$bot_name
+dr=$(ls $pathtobot/$user_id/$bot_name)
+mv $pathtobot/$user_id/$bot_name/$dr/* $pathtobot/$user_id/$bot_name/
 # create virtualenv and install essensial modules
-python3 -m venv ../bots/$user_id/$bot_name/venv
-source ../bots/$user_id/$bot_name/venv/bin/activate
-pip3 install -r ../bots/$user_id/$bot_name/requirements.txt
-rm -rf ../bots/$user_id/$bot_name/$dr 
+python3 -m venv $pathtobot/$user_id/$bot_name/venv
+source $pathtobot/$user_id/$bot_name/venv/bin/activate
+pip3 install -r $pathtobot/$user_id/$bot_name/requirements.txt
+rm -rf $pathtobot/$user_id/$bot_name/$dr 
 exit 0
