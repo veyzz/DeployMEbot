919511c2afea67eaf68ff95cd7c4940df04f6210
veyzz <munir.taga@gmail.com>
2019-06-13 04:21:18 +0300

    added logging

--- backend/preparefiles.sh
+++ backend/preparefiles.sh
@@ -6,24 +6,38 @@ pathtobot=$3
 bot_name=${arch%.zip}
 
 # make new dir and clean old files
-echo $pathtobot/bots/$user_id
-mkdir $pathtobot/bots/$user_id
-mkdir $pathtobot/bots/$user_id/$bot_name
+if ! [ -d $pathtobot/bots/$user_id ]; then
+    echo "Creating user directory..."
+    mkdir $pathtobot/bots/$user_id
+fi
+if ! [ -d $pathtobot/bots/$user_id/$bot_name ]; then
+    echo "Creating bot directory..."
+    mkdir $pathtobot/bots/$user_id/$bot_name 
+fi
+echo "Cleaning bot folder..."
 rm -rf $pathtobot/bots/$user_id/$bot_name/*
 
 # unzip files
+echo "Unpacking $arch"
 unzip $pathtobot/download/$user_id/$arch -d $pathtobot/bots/$user_id/$bot_name
 dr=$(ls $pathtobot/bots/$user_id/$bot_name)
 mv $pathtobot/bots/$user_id/$bot_name/$dr/* $pathtobot/bots/$user_id/$bot_name/
+echo "Unpacked to bots/$user_id/$bot_name/"
 
 # create virtualenv and install essensial modules
+echo "Creating virtual environment..."
 python3 -m venv $pathtobot/bots/$user_id/$bot_name/venv
 source $pathtobot/bots/$user_id/$bot_name/venv/bin/activate
+echo "Installing essensial modules..."
 pip3 install -r $pathtobot/bots/$user_id/$bot_name/requirements.txt
+echo "Removing trash directory..."
 rm -rf $pathtobot/bots/$user_id/$bot_name/$dr
+echo "Creating log directory..."
 mkdir $pathtobot/bots/$user_id/$bot_name/log
 
 # create sh files
+echo "Creating control-file..."
 cat $pathtobot/backend/templates/bot > $pathtobot/bots/$user_id/$bot_name/bot.sh
 
+echo "Done"
 exit 0
