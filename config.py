f06c1b87c97d8749a45937f846d7b4c08a07cc07
veyzz <munir.taga@gmail.com>
2019-06-14 05:05:29 +0300

    edited const

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 mode = 0
 token = "763561450:AAEoNo-MUcCMdhkhk3Mk1RtQYlRzKCjxWms"
 db = 'deploymebot.db'
-bots_count = 2
+bots_count = 1
 proxy = {
     'http': '203.246.112.133:3128',
     'https': '23.237.22.82:3128'
