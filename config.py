f748eb13dc3aba529a5b122cdb164d5ce21a60f4
veyzz <munir.taga@gmail.com>
2019-06-18 21:54:28 +0300

    added commands

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ mode = 0
 token = "763561450:AAEoNo-MUcCMdhkhk3Mk1RtQYlRzKCjxWms"
 db = 'deploymebot.db'
 bots_count = 1
+commands = ['start', 'stop', 'restart', 'logs']
 proxy = {
     'http': '176.9.192.249:8888',
     'https': '176.9.192.249:8888'
