f77caf6db85610b7d392680cc5e743e21791c437
veyzz <munir.taga@gmail.com>
2019-06-26 00:03:11 +0300

    added interaction with db

--- backend/removefiles.sh
+++ backend/removefiles.sh
@@ -2,18 +2,27 @@
 # This script removes all files from bot folder
 
 # read args
-path=$1
+bot_id=$1
+path=$2
+
+# work with db
+bot=$(echo "SELECT * FROM bots WHERE id = $bot_id" | sqlite3 $path/deploymebot.db)
+echo "DELETE FROM bots WHERE id = $bot_id" | sqlite3 $path/deploymebot.db
+
+user_id=$(echo $bot | awk -F'|' '{print $5}')
+bot_name=$(echo $bot | awk -F'|' '{print $2}')
+bot_folder=$path/bots/$user_id/$bot_name
+echo $bot_folder
 
 # check if exist this directory
-if ! [ -d $path ]; then
+if ! [ -d $bot_folder ]; then
     echo "This directory does not exist"
-    rm $log_file
     exit 1
 fi
 
 # remove folder
 echo "Removing bot directory..."
-rm -rfv $path
+rm -rfv $bot_folder
 
 echo "Done"
 exit 0
