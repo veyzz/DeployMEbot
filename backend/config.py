30f51ce98a208563c6e1d948e59589e3d73afbd1
veyzz <munir.taga@gmail.com>
2020-04-05 18:12:28 +0300

    added config

---
+++ backend/config.py
@@ -0,0 +1,9 @@
+from backend import proxy
+
+mode = 1
+token = ###YOUR TOKEN HERE###
+admins = [###ID's OF ADMINS###]
+feedback_channel = ###ID###
+db = 'deploymebot.db'
+bots_count = 2
+commands = ['start', 'stop', 'logs', 'remove']
